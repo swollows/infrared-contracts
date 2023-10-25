176f4d772f21b9f2cc5238b1d3648823369ccd21
red4626 <red@infrared.finance>
2023-10-25 17:25:59 +0400

    feat: addresses

---
+++ contracts/src/script/devnet/Addresses.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Script, console2} from 'forge-std/Script.sol';
+
+// Contract that keeps track of a given deployment of the contracts.
+contract Addreses {
+    // Core contracts.
+    address public ibgt;
+    address public infrared;
+    address public wibgt;
+    address public wibgtVault;
+
+    function setIbgt(address _ibgt) external {
+        ibgt = _ibgt;
+    }
+
+    function setInfrared(address _infrared) external {
+        infrared = _infrared;
+    }
+
+    function setWibgt(address _wibgt) external {
+        wibgt = _wibgt;
+    }
+
+    function setWibgtVault(address _wibgtVault) external {
+        wibgtVault = _wibgtVault;
+    }
+}
+
+// Script top deploy the addresses contract.
+contract DeployAddresses is Script {
+    function run() public {
+        vm.startBroadcast();
+
+        new Addreses();
+
+        vm.stopBroadcast();
+    }
+}
---
+++ contracts/src/script/devnet/Configuration.sol
@@ -0,0 +1,17 @@
+// // SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+library Validators {
+    address public constant VAL_0 = 0x6c1a8ef7ead982bfc52693976b8F11C0fA6ef688;
+    address public constant VAL_1 = 0x8aFaBfD41E8C6489812F4457e8d8DfcF75EbaA32;
+    address public constant VAL_2 = 0x8F0C8170ec6bc4ff0fFC3cbCf87D1A2666924725;
+    address public constant VAL_3 = 0xfd8106B6917c8F593a3145b44e0d6a2251C72Ba2;
+}
+
+library Precompiles {
+    address public constant BANK_PRECOMPILE = 0x4381dC2aB14285160c808659aEe005D51255adD7;
+    address public constant DISTRIBUTION_PREOCMPILE = 0x0000000000000000000000000000000000000069;
+    address public constant REWARDS_PRECOMPILE = 0x55684e2cA2bace0aDc512C1AFF880b15b8eA7214;
+    address public constant STAKING_PRECOMPILE = 0xd9A998CaC66092748FfEc7cFBD155Aae1737C2fF;
+    address public constant ERC20_PRECOMPILE = 0x0000000000000000000000000000000000696969;
+}
